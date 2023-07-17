c5aeed01ca0bd38127982253c5c3ba612f23e8ee
eagel27 <eirini.angeloudi@gmail.com>
2023-07-17 14:37:07 +0100

    Include tqdm in installation requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='illustris_python',
     version='1.0.0',
     packages=["illustris_python"],
-    install_requires=["numpy", "h5py", "six"],
+    install_requires=["numpy", "h5py", "six", "tqdm"],
     tests_require=["nose","coverage"],
 )
